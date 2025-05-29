b968497e1daf9871771eda5006c96ca0ed608c4e
buitronic30 <sabp3008@gmail.com>
2025-05-29 14:22:36 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>¡Autoanálisis Facial en React!</h1>
+      <p>Aquí irá tu aplicación.</p>
+    </div>
+  );
+}
+
+export default App;
